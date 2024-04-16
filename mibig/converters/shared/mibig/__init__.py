199958e57726473700e1bba283ecf797f417c6ad
Kai Blin <kblin@biosustain.dtu.dk>
2024-04-16 14:29:16 +0200

    shared/mibig: Validate Locus and Taxonomy against the record if possible
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- mibig/converters/shared/mibig/__init__.py
+++ mibig/converters/shared/mibig/__init__.py
@@ -2,6 +2,7 @@ from typing import Any, Self
 
 from mibig.converters.shared.common import Location, Citation
 from mibig.errors import ValidationError
+from mibig.utils import Record
 from mibig.validation import ValidationErrorInfo
 
 
@@ -56,7 +57,8 @@ class Locus:
     location: Location
     evidence: list[LocusEvidence]
 
-    def __init__(self, accession: str, location: Location, evidence: list[LocusEvidence], validate: bool = True) -> None:
+    def __init__(self, accession: str, location: Location, evidence: list[LocusEvidence],
+                 validate: bool = True, **kwargs) -> None:
         self.accession = accession
         self.location = location
         self.evidence = evidence
@@ -64,17 +66,22 @@ class Locus:
         if not validate:
             return
 
-        errors = self.validate()
+        errors = self.validate(**kwargs)
         if errors:
             raise ValidationError(errors)
 
-    def validate(self) -> list[ValidationErrorInfo]:
+    def validate(self, **kwargs) -> list[ValidationErrorInfo]:
         errors: list[ValidationErrorInfo] = []
 
-        if self.accession.count(".") > 1:
-            errors.append(ValidationErrorInfo("Locus.accession", f"Invalid accession {self.accession}"))
+        if "record" in kwargs:
+            record: Record = kwargs["record"]
+            if record.id != self.accession:
+                errors.append(ValidationErrorInfo("Locus.accession", f"Accession mismatch: {self.accession} != {record.id}"))
+        else:
+            if self.accession.count(".") > 1:
+                errors.append(ValidationErrorInfo("Locus.accession", f"Invalid accession {self.accession}"))
 
-        errors.extend(self.location.validate())
+        errors.extend(self.location.validate(**kwargs))
 
         for evidence in self.evidence:
             errors.extend(evidence.validate())
@@ -82,9 +89,9 @@ class Locus:
         return errors
 
     @classmethod
-    def from_json(cls, raw: dict[str, Any]) -> Self:
+    def from_json(cls, raw: dict[str, Any], **kwargs) -> Self:
         evidence: list[LocusEvidence] = [LocusEvidence.from_json(e) for e in raw["evidence"]]
-        return cls(raw["accession"], Location.from_json(raw["location"]), evidence)
+        return cls(raw["accession"], Location.from_json(raw["location"], **kwargs), evidence)
 
     def to_json(self) -> dict[str, Any]:
         return {
@@ -98,27 +105,32 @@ class Taxonomy:
     name: str
     ncbi_tax_id: int
 
-    def __init__(self, name: str, ncbi_tax_id: int, validate: bool = True) -> None:
+    def __init__(self, name: str, ncbi_tax_id: int, validate: bool = True, **kwargs) -> None:
         self.name = name
         self.ncbi_tax_id = ncbi_tax_id
 
         if not validate:
             return
 
-        errors = self.validate()
+        errors = self.validate(**kwargs)
         if errors:
             raise ValidationError(errors)
 
-    def validate(self) -> list[ValidationErrorInfo]:
+    def validate(self, **kwargs) -> list[ValidationErrorInfo]:
         errors: list[ValidationErrorInfo] = []
 
-        # TODO: Figure out if we can validate this somehow
+        if "record" in kwargs:
+            record: Record = kwargs["record"]
+            if record.organism != self.name:
+                errors.append(ValidationErrorInfo("Taxonomy.name", f"Name mismatch: {self.name} != {record.organism}"))
+            if record.ncbi_tax_id != self.ncbi_tax_id:
+                errors.append(ValidationErrorInfo("Taxonomy.ncbi_tax_id", f"NCBI Tax ID mismatch: {self.ncbi_tax_id} != {record.ncbi_tax_id}"))
 
         return errors
 
     @classmethod
-    def from_json(cls, raw: dict[str, Any]) -> Self:
-        return cls(raw["name"], raw["ncbi_tax_id"])
+    def from_json(cls, raw: dict[str, Any], **kwargs) -> Self:
+        return cls(raw["name"], raw["ncbi_tax_id"], **kwargs)
 
     def to_json(self) -> dict[str, Any]:
         return {
