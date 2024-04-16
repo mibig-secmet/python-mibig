9202a1b695d517dd9b1f18cbe7c828d5e17d64a8
Kai Blin <kblin@biosustain.dtu.dk>
2024-04-16 15:06:20 +0200

    errors: Make ValidationErrors easier to debug
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- mibig/errors.py
+++ mibig/errors.py
@@ -10,3 +10,6 @@ class ValidationError(MibigError):
     def __init__(self, info: list[ValidationErrorInfo]) -> None:
         super().__init__()
         self.info = info
+
+    def __str__(self) -> str:
+        return "\n".join(str(e) for e in self.info)
