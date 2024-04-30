70d2430455f76ae9ad001f8dce0fb6f05132e86e
Kai Blin <kblin@biosustain.dtu.dk>
2024-04-30 08:27:53 +0200

    shared/steps: Change the syntax and description to be more in line with what was initially proposed
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- mibig/converters/shared/mibig/biosynthesis/steps.py
+++ mibig/converters/shared/mibig/biosynthesis/steps.py
@@ -1,18 +1,21 @@
 # steps have the following format:
-# A -> B (first enzyme A acts, then enzyme B acts)
-# A/B (enzyme A and B act simultaneously, or in an unknown order)
-# instead of enzymes, module names can be used as well
-# 1 -> 2/A -> 3 (module 1 acts, then module 2 and A act simultaneously, then module 3 acts)
+
+# Commas signify an unordered path: exA, exB, exC
+# greater-than signs signify an ordered path: exA > exB > exC
+# To indicate modules, use square brackets: [modA]
+# To indicate cooperation, use a plus sign: exA + exB
+# To indicate two enzymes being interchangeable, use a pipe symbol: exA | exB
+# A trans-AT can be written like this [modA] + exB
 # TODO: how to deal with multiple ordered steps that themsselves are in unknown order?
 
 def parse_step(step_description: str) -> list[tuple[str, ...]]:
-    steps = step_description.split("->")
+    steps = step_description.split(">")
     parsed_steps = []
     for step in steps:
-        parsed_steps.append(tuple(map(str.strip, step.split("/"))))
+        parsed_steps.append(tuple(map(str.strip, step.split(","))))
     return parsed_steps
 
 
 def format_step(steps: list[tuple[str, ...]]) -> str:
-    return " -> ".join("/".join(step) for step in steps)
+    return " > ".join("/".join(step) for step in steps)
 
